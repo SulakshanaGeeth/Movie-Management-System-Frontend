6a78d5874f8a37c3c102b29cd8797a98ebd3412d
Sulakshana Geeth <sulakshanageeth@gmail.com>
2021-12-13 14:32:13 +0530

    Build the sorting component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 // import Movies from "./components/movies";
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
 // class App extends Component {
 //   state = {
