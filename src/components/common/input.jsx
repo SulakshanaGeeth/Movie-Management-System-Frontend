3d2b3b30867230b638f80b28b66b30933040ac1f
Sulakshana Geeth <sulakshanageeth@gmail.com>
2021-12-18 13:18:25 +0530

    Extracting a Reusable Input.

---
+++ src/components/common/input.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+const Input = ({ name, lable, value, onChange }) => {
+  return (
+    <div className="form-group">
+      <label htmlFor={name}>{lable}</label>
+      <input
+        autoFocus
+        value={value}
+        onChange={onChange}
+        id={name}
+        name={name}
+        type="text"
+        className="form-control"
+      />
+    </div>
+  );
+};
+
+export default Input;
