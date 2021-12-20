0810c0276e00f5055670e41e402236e317ec69bf
Sulakshana Geeth <sulakshanageeth@gmail.com>
2021-12-20 06:48:09 +0530

    Build the registration form component.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
 class App extends Component {
@@ -21,6 +22,7 @@ class App extends Component {
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/register" component={RegisterForm}></Route>
             <Route path="/not-found" component={NotFound}></Route>
             <Redirect from="/" exact to="/movies" />
             <Redirect to="/not-found" />
