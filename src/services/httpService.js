a64f28ea9a40483a23e74bcc621fbe2b2d3878e8
Sulakshana Geeth <sulakshanageeth@gmail.com>
2021-12-22 09:32:11 +0530

    Replacing FakeGenreService.

---
+++ src/services/httpService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import logger from "./logService";
+import { toast } from "react-toastify";
+
+axios.interceptors.response.use(null, (error) => {
+  const expectedErrror =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedErrror) {
+    logger.log(error);
+    toast.error("An unexpected error occurred.");
+  }
+
+  return Promise.reject(error);
+});
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  delete: axios.delete,
+};
