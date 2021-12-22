6a6d81a33394dbf094d47e233ba03ca7e6daad17
Sulakshana Geeth <sulakshanageeth@gmail.com>
2021-12-22 21:31:37 +0530

    Logging out a User.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
+import Logout from "./components/logout";
 import RegisterForm from "./components/registerForm";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
@@ -31,12 +32,13 @@ class App extends Component {
         <NavBar user={this.state.user} />
         <main className="container">
           <Switch>
+            <Route path="/register" component={RegisterForm}></Route>
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
             <Route path="/rentals" component={Rentals}></Route>
-            <Route path="/register" component={RegisterForm}></Route>
             <Route path="/not-found" component={NotFound}></Route>
             <Redirect from="/" exact to="/movies" />
             <Redirect to="/not-found" />
